ff83de98310bdaf7309957c66fd63089dd4653d3
Sebastian Waldbauer OskarSniper <sebi.developer@gmail.com>
2020-06-23 14:07:51 +0200

    Export in global index
    Signed-off-by: Sebastian Waldbauer OskarSniper <sebi.developer@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { on, log, emit } from "alt-client";
 import { Module } from "./module";
 
+export * from './module';
 export function InitModule(name: string, mod: Module) {
     on('connectionComplete', () => {
         emit('rp:module:init', [name]);
-        on(`rp:module:${name}:load`, (ok) => {
+        on(`rp:module:${name}:load`, (ok: boolean) => {
             if(ok) {
                 mod.emit(`m:load`);
             }
